29b37b81acbf046cf98b2ba7293e46a3ebd0d85c
gabrielarck <stanislasvega@yahoo.fr>
2019-10-21 10:06:55 +0200

    traitement de l'api en cours

--- src/components/MyApp.js
+++ src/components/MyApp.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './MyApp.css';
 
-function MyApp() {
-  return (
-    <div className="">
-      
-  </div>
 
-  );
+class MyApp extends Component{
+  state = {
+    post : {}
+  }
+
+  componentDidMount(){
+    fetch("https://api.themoviedb.org/3/movie/14?api_key=7f077937236d1ffe1a9deeb64a9d2a38&&append_to_response=video,image")
+    .then((response) => {
+      return response.json()  
+      })
+      .then((result) => {
+          this.setState({post: result})
+       console.log(result)
+    })  
+    }
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.post.original_title}</h1>
+        {this.state.post.overview ? this.state.post.overview : <p>chargement en court...</p> }
+        <img src="/4iJfYYoQzZcONB9hNzg0J0wWyPH.jpg" alt="affiche"></img>
+        {this.state.post.poster_path}
+      
+    </div>
+    );
+  }
 }
+
+
+
 export default MyApp;
