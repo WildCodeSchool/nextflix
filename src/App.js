5b0b8eee21b7d68bedc96ca7b22d5c6481901b42
cassandra37 <avenelcassandra@outlook.com>
2019-10-22 10:56:12 +0200

    Fetch data from API

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
-  <Wall/>
-
-
+      <Wall/>
     </div>
   );
 }
--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Wall(){
-    return(    
-        <div className="wall">
+class Wall extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&&api_key=7f077937236d1ffe1a9deeb64a9d2a38&&append_to_response=video,image")
+            .then(response => response.json())
+            .then(data => this.setState({ videos: data.results }));
+    }
+
+    render() {
+        return (
+            <div className="wall">
             
-            <div className="section">
-                <h2>The 100 saison 1</h2>
-                <iframe width="760" height="515" src="https://www.youtube.com/embed/qj8kHtRawHY" frameborder="0" title="The 100" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                {this.state.videos.map((video) => (
+                    <div className="section">
+                        <h2>{video.title}</h2>
+                        <iframe 
+                            width="760" height="515" 
+                            src="https://www.youtube.com/embed/qj8kHtRawHY" 
+                            frameBorder="0" title="The 100" 
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                            allowFullScreen></iframe>
+                    </div>
+                ))}
+                
             </div>
-
-            <div className="section">
-                <h2>Elite saison 1</h2>
-                <iframe width="760" height="515" src="https://www.youtube.com/embed/99Q-va5jLbY" frameborder="0" title="Elite" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-
-            <div className="section">
-                <h2>STRANGER THINGS saison 1</h2>
-                <iframe width="760" height="515" src="https://www.youtube.com/embed/wQoHfKDncLU" frameborder="0" title="Stranger" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-            
-        </div>
-    )
+        )
+    }
 }
 
 export default Wall;
