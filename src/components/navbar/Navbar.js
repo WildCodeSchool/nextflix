bc3527eaf4ef66aff958ffdcc87e1e80bc32a35e
alexiadb <alexiadeboynes@gmail.com>
2019-10-24 09:23:10 +0200

    finished nav

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar () {
+    return (
+
+            <div className ='navi'>
+              <nav>
+              
+              <img alt='nextflic-logo' src='../../../images/NETFLIX.png'></img>
+
+            
+              
+              </nav>
+          </div>
+        )
+        
+
+    }
+
+export default Navbar;
