958d2806387e6144809b53d3773fe7bb7f9ad337
gabrielarck <stanislasvega@yahoo.fr>
2019-10-29 09:42:26 +0100

    modification PR

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/navbar/Navbar.js';
-import Wall from './components/Wall';
+import React from ' react ';
+import ' ./App.css ';
+import Navbar from ' ./components/navbar/Navbar.js ';
+import Wall from ' ./components/Wall ';
 function App() {
   return (
     <div className="App">
