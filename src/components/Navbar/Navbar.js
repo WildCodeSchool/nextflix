4f44b46d5b3ea13af9fcefdade1aa74f7ef29650
gabrielarck <stanislasvega@yahoo.fr>
2019-11-06 11:57:45 +0100

    fix coding style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,27 +1,25 @@
-import React ,{Component}from 'react';
+import React from 'react';
 import './Navbar.css';
 
 
-class Navbar extends Component {
-	render(){
-	    return (
-            <div className ='navi'>
-              <nav>
-              <div className = {this.props.show ? "nburger-show" : "nburger-hidden"} onClick={() => this.props.changeNavBar()}>
-              	<img alt='nextflic-logo' src='../../../images/NETFLIX.png' />
-                 <ul>
-                  <li>Movies</li>
-                  <li>Series</li>
-                  <li>My Favorite</li>
-                </ul>
-              </div>
-            
-              
-              </nav>
-          </div>
-        )
-     }   
+function Navbar({ show, changeNavBar }) {
+  return (
+    <div className="navi">
+      <nav>
+        <div 
+          className={show ? "nburger-show" : "nburger-hidden"} 
+          onClick={() => changeNavBar()}
+        >
+          <img alt="nextflic-logo" src="../../../images/NETFLIX.png" />
+          <ul>
+            <li>Movies</li>
+            <li>Series</li>
+            <li>My Favorite</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
-
