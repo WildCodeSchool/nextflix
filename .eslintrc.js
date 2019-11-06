b2afdf639124378a8b8934b246d39a17de624ad4
gabrielarck <stanislasvega@yahoo.fr>
2019-11-06 14:54:35 +0100

    fix coding style

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,11 @@ module.exports = {
     extends : 'airbnb'  ,
     rules : {
         'react/jsx-filename-extension' : [1, { extensions : ['.js']}],
+        'no-plusplus': 'off'
     },
+    globals: {
+        "fetch": true,
+        "document": true,
+        "window": true
+    }
 };
--- src/components/Wall/Wall.js
+++ src/components/Wall/Wall.js
@@ -1,76 +1,102 @@
-import React, { Component } from 'react';
-import './Wall.css'; 
+import React, { Component } from 'react';
+import './Wall.css';
 
+class Wall extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      trailer: [],
+      scrollIndex: 1,
+    };
+  }
 
+  /* titre */
 
-class Wall extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            videos: [],
-            trailer:[],
-            scrollIndex: 1
-        }
-    }
-
-    /* titre */
-
-    async componentDidMount() {
-        const response = await fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&&api_key=7f077937236d1ffe1a9deeb64a9d2a38&&append_to_response=video,image")
-        const data = await response.json();
-        console.log(data);
+  async componentDidMount() {
+    const response = await fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&&api_key=7f077937236d1ffe1a9deeb64a9d2a38&&append_to_response=video,image');
+    const data = await response.json();
 
     /* video */
 
-        const trailer=[];
-        for(let i=0 ; i < data.results.length; i++){ 
-            const response2 = await fetch(`https://api.themoviedb.org/3/movie/${data.results[i].id}/videos?api_key=7f077937236d1ffe1a9deeb64a9d2a38`)
-            const data2 = await response2.json();
-            trailer.push(data2.results);           
-        }
-        this.setState({ videos: data.results, trailer});
+    const promises = [];
+    for (let i = 0; i < data.results.length; i++) {
+      const request = fetch(`https://api.themoviedb.org/3/movie/${data.results[i].id}/videos?api_key=7f077937236d1ffe1a9deeb64a9d2a38`);
+      promises.push(request);
     }
 
-    scrollMovieDown(){
-        const height = document.querySelector(".video").offsetHeight;
-        this.setState( prevState => ({scrollIndex: prevState.scrollIndex +1}));
-        const positionIndex = this.state.scrollIndex * height;
-        window.scrollTo(0,positionIndex)
-    }
+    const responses = await Promise.all(promises);
+    const datas = await Promise.all(responses.map((res) => res.json()));
+    const trailer = datas.map((d) => d.results);
 
-    scrollMovieUp(){
-        const height = document.querySelector(".video").offsetHeight;
-        this.setState( prevState => ({scrollIndex: prevState.scrollIndex -1}));
-        const positionIndex = this.state.scrollIndex * height;
-        window.scrollTo(0,positionIndex)
-    }
+    this.setState({ videos: data.results, trailer });
+  }
 
-    /* regroupe titre video */
+  scrollMovieDown() {
+    const { scrollIndex } = this.state;
+    const height = document.querySelector('.video').offsetHeight;
+    this.setState((prevState) => ({ scrollIndex: prevState.scrollIndex + 1 }));
+    const positionIndex = scrollIndex * height;
+    window.scrollTo(0, positionIndex);
+  }
 
-    render() {
-        return (
-            <div className="wall">
-            <div className="button-down">
-            <button className="buttonDown" onClick={() => this.scrollMovieDown() }><img src="https://i.imgur.com/iMNydQb.png"alt="arrow down" className="arrow-down"></img></button> 
-            </div>
-            <div className="button-up">
-            <button className="buttonUp" onClick={() => this.scrollMovieUp() }><img src="https://i.imgur.com/yw7ymJE.png" alt="arrow up" className="arrow-up"></img></button>
-            </div>            
-                {this.state.videos.map((video, i) => (
-                    <div className="section">
-                        {/* <h2 className="trailertitle">{video.title}</h2> */}
-                        <iframe className="video"
-                            width="760" height="515" 
-                            src={`https://www.youtube.com/embed/${this.state.trailer[i][0].key}`} 
-                            frameBorder="0" title="The 100" 
-                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                            allowFullScreen></iframe>
-                    </div>
-                ))}
-                
-            </div>
-        )
-    }
+  scrollMovieUp() {
+    const { scrollIndex } = this.state;
+    const height = document.querySelector('.video').offsetHeight;
+    this.setState((prevState) => ({ scrollIndex: prevState.scrollIndex - 1 }));
+    const positionIndex = scrollIndex * height;
+    window.scrollTo(0, positionIndex);
+  }
+
+  /* regroupe titre video */
+
+  render() {
+    const { videos, trailer } = this.state;
+    return (
+      <div className="wall">
+        <div className="button-down">
+          <button
+            className="buttonDown"
+            type="button"
+            onClick={() => this.scrollMovieDown()}
+          >
+            <img
+              src="https://i.imgur.com/iMNydQb.png"
+              alt="arrow down"
+              className="arrow-down"
+            />
+          </button>
+        </div>
+        <div className="button-up">
+          <button
+            type="button"
+            className="buttonUp"
+            onClick={() => this.scrollMovieUp()}
+          >
+            <img
+              src="https://i.imgur.com/yw7ymJE.png"
+              alt="arrow up"
+              className="arrow-up"
+            />
+          </button>
+        </div>
+        {videos.map((video, i) => (
+          <div className="section">
+            <iframe
+              className="video"
+              width="760"
+              height="515"
+              src={`https://www.youtube.com/embed/${trailer[i][0].key}`}
+              frameBorder="0"
+              title="The 100"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+              allowFullScreen
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Wall;
