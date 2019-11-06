e0d205fd086b6b911a542fedd543a086ca6ca30f
gabrielarck <stanislasvega@yahoo.fr>
2019-11-06 11:36:53 +0100

    fix indentation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React , {Component}from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Navbar from './components/navbar/Navbar.js';
+import Navbar from './components/navbar/Navbar';
 import Wall from './components/Wall';
 
 class App extends Component {
 	constructor(props){
 		super(props)
-		this.state={
+		this.state = {
 			showNavBar: false
 		}
 	}
 	changeNavBar = () => {
-		this.setState({showNavBar: !this.state.showNavBar})
+		this.setState({ showNavBar: !this.state.showNavBar })
 	}
 	render(){
-  return (
-    <div className="App">
-      <Navbar show={this.state.showNavBar} changeNavBar={this.changeNavBar}/>
-      <Wall/>
-    </div>
-
-  );
-  }
+		return (
+			<div className="App">
+				<Navbar show={this.state.showNavBar} changeNavBar={this.changeNavBar}/>
+				<Wall/>
+			</div>
+  		);
+	}
 }
 
 export default App;
